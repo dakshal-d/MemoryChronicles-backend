d420c96df9c48b0e3cb86e7693083d5c21173b5c
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-09-23 02:11:03 +0530

    updated

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-const uri= MONGO_CONNECTION_URL;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+const connection = mongoose.connect(`mongodb+srv://ddalsania2002:2UFmjd3ycD3Su5c3@cluster1.uxpoepb.mongodb.net/?retryWrites=true&w=majority`);
 module.exports = {
   connection,
 };
