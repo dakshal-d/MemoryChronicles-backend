e27600c9180d9ff4490db2d94ebefa3e038ca88f
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-09-23 01:45:13 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const uri = process.env.MONGODB_URI;
-const connection = mongoose.connect(uri);
+const connection = mongoose.connect(`mongodb+srv://ddalsania2002:2UFmjd3ycD3Su5c3@cluster1.uxpoepb.mongodb.net/?retryWrites=true&w=majority`);
 module.exports = {
   connection,
 };
